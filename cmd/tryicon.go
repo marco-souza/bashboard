7c8825ea178f90917a952b4e83681ba93192b92c
Marco Antônio <ma.souza.junior@gmail.com>
2022-01-25 22:27:22 -0300

    fea: add try icon

---
+++ cmd/tryicon.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/getlantern/systray"
+	"github.com/getlantern/systray/example/icon"
+)
+
+func StartApp(done *chan bool) {
+	onExit := func() {
+		fmt.Println("Exiting...")
+		*done <- true
+	}
+
+	go systray.Run(onReady, onExit)
+}
+
+func onReady() {
+	// Sys tray icon
+	systray.SetTemplateIcon(icon.Data, icon.Data)
+	systray.SetTitle("Webview example")
+
+	// Menu items
+	mShowLantern := systray.AddMenuItem("Show Lantern", "")
+	mShowWikipedia := systray.AddMenuItem("Show Wikipedia", "")
+	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
+
+	registerClickHandlers := func() {
+		for {
+			select {
+			// TODO: show window
+			case <-mShowLantern.ClickedCh:
+				fmt.Println("Lantern")
+			case <-mShowWikipedia.ClickedCh:
+				fmt.Println("Wiki")
+			case <-mQuit.ClickedCh:
+				systray.Quit()
+			}
+		}
+	}
+	go registerClickHandlers()
+}
--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"marco-souza/binance-dashboard/cmd"
 )
 
 func main() {
+	done := make(chan bool)
 	// Start static server
 	go cmd.StartPageServer("8001")
 
-	cmd.StartDesktopApp() // sync / blocking
+	go cmd.StartApp(&done) // sync / blocking
+
+	fmt.Println("Waiting to finish")
+
+	<-done
+	fmt.Println("Finishing")
 }
