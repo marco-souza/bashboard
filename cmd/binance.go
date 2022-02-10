f681964b505a0a3e0df9bf93417dedfd8606d52c
Marco Antônio <ma.souza.junior@gmail.com>
2022-02-10 14:48:45 -0300

    enh: improve binance api reusability
    - create fetch function
    - make a shared makeRequest function

--- cmd/binance.go
+++ cmd/binance.go
@@ -24,25 +24,13 @@ type SystemStatusResponse struct {
 
 func FetchSystemStatus() *SystemStatusResponse {
 	url := getBinanceEndpoint("system-status")
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		panic(err)
-	}
+	req := makeRequest(url, "")
 
 	// Send the request
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-
-	// Parse responseBody
-	responseBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
+	responseBody := fetch(req)
 
 	var systemStatus SystemStatusResponse
-	if err = json.Unmarshal(responseBody, &systemStatus); err != nil {
+	if err := json.Unmarshal(responseBody, &systemStatus); err != nil {
 		panic(err)
 	}
 
@@ -62,7 +50,7 @@ type AccountData struct {
 
 type AccountSnapshot struct {
 	Type       string      `json:"type"`
-	UpdateTime int   `json:"updateTime"`
+	UpdateTime int         `json:"updateTime"`
 	Data       AccountData `json:"data"`
 }
 
@@ -74,9 +62,20 @@ type AccountSnapshotResponse struct {
 
 func FetchAccountSnapshot() *AccountSnapshotResponse {
 	url := getBinanceEndpoint("account-snapshot")
-	params := fmt.Sprintf("type=SPOT&endTime=%d", time.Now().Unix() * 1000) // params: https://binance-docs.github.io/apidocs/spot/en/#daily-account-snapshot-user_data
+	params := fmt.Sprintf("type=SPOT&endTime=%d", time.Now().Unix()*1000) // params: https://binance-docs.github.io/apidocs/spot/en/#daily-account-snapshot-user_data
 	req := makeSignedRequest(url, params)
 
+	responseBody := fetch(req)
+
+	var accountSnapshot AccountSnapshotResponse
+	if err := json.Unmarshal(responseBody, &accountSnapshot); err != nil {
+		panic(err)
+	}
+
+	return &accountSnapshot
+}
+
+func fetch(req *http.Request) []byte {
 	log.Println("RequestURL: ", req.URL)
 
 	// Send the request
@@ -93,12 +92,7 @@ func FetchAccountSnapshot() *AccountSnapshotResponse {
 
 	log.Println("ResposeBody", string(responseBody))
 
-	var accountSnapshot AccountSnapshotResponse
-	if err = json.Unmarshal(responseBody, &accountSnapshot); err != nil {
-		panic(err)
-	}
-
-	return &accountSnapshot
+	return responseBody
 }
 
 var endpoint = map[string]string{
@@ -109,18 +103,26 @@ var endpoint = map[string]string{
 func getBinanceEndpoint(name string) string {
 	url := endpoint[name]
 	if url == "" {
-		panic("No endpoint found")
+		panic(fmt.Sprintf("No '%s' endpoint found", name))
 	}
 	return BASE_BINANCE_URL + url
 }
 
 func makeSignedRequest(url, params string) *http.Request {
-	req, err := http.NewRequest("GET", signUrl(url, params), nil)
+	// TODO handle query params properly
+	req := makeRequest(signUrl(url, params), "")
+	req.Header.Set("X-MBX-APIKEY", apiKey)
+	return req
+}
+
+func makeRequest(url, params string) *http.Request {
+	if params != "" {
+		url += "?" + params
+	}
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		panic(err)
 	}
-
-	req.Header.Set("X-MBX-APIKEY", apiKey)
 
 	return req
 }
