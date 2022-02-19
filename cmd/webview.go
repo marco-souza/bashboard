0d9338070d899a8059ae112df8d491dedb3413a7
Marco Antônio <ma.souza.junior@gmail.com>
2022-02-19 15:25:29 -0300

    fix: use lorca

--- cmd/webview.go
+++ cmd/webview.go
@@ -2,20 +2,42 @@ package cmd
 
 import (
 	"fmt"
+	"net/url"
 
-	"github.com/webview/webview"
+	"github.com/zserge/lorca"
 )
 
-func StartDesktopApp() {
-	debug := true
-	w := webview.New(debug)
-	defer w.Destroy()
-	fmt.Println("Starting desktop app")
+var ui lorca.UI
 
-	// setup window
-	w.SetTitle("Bashboard")
-	w.SetSize(800, 600, webview.HintNone)
-	w.Navigate("http://localhost:" + PORT)
+func OpenMain() {
+	ui, err := lorca.New("data:text/html,"+url.PathEscape(`
+	<html>
+		<head><title>Hello</title></head>
+		<body><h1>Hello, world!</h1></body>
+	</html>
+	`), "", 480, 320)
+	if err != nil {
+		panic(err)
+	}
+	defer ui.Close()
 
-	w.Run()
+	// Bind Go function to be available in JS. Go function may be long-running and
+	// blocking - in JS it's represented with a Promise.
+	ui.Bind("add", func(a, b int) int { return a + b })
+
+	// Call JS function from Go. Functions may be asynchronous, i.e. return promises
+	n := ui.Eval(`Math.random()`).Float()
+	fmt.Println(n)
+
+	// Call JS that calls Go and so on and so on...
+	m := ui.Eval(`add(2, 3)`).Int()
+	fmt.Println(m)
+
+	// Wait for the browser window to be closed
+	<-ui.Done()
+}
+
+func CloseMain() {
+	// mainWindow.Destroy()
+	ui.Close()
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.17
 
 require (
 	github.com/getlantern/systray v1.1.0
-	github.com/webview/webview v0.0.0-20210330151455-f540d88dde4e
+	github.com/zserge/lorca v0.1.10
 )
 
 require (
@@ -16,5 +16,6 @@ require (
 	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
+	golang.org/x/net v0.0.0-20200222125558-5a598a2470a0 // indirect
 	golang.org/x/sys v0.0.0-20201018230417-eeed37f84f13 // indirect
 )
